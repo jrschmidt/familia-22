8396f3536168c43e336f2f5a221b8bd9e7b300f0
jrschmidt <jrschmidt56@gmail.com>
2022-01-16 15:04:35 -0700

    add TreeViewBox and TreeViewRow components

---
+++ src/components/TreeViewBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tree-view-box">
+    <h5>TreeViewBox component</h5>
+    <TreeViewRow/>
+  </div>
+</template>
+
+<script>
+  import TreeViewRow from './TreeViewRow.vue'
+
+  export default {
+    components: {
+      TreeViewRow
+    }
+  }
+</script>
+
+<style lang="css">
+  .tree-view-box {
+    border-color: maroon;
+    border-style: dotted;
+  }
+</style>
---
+++ src/components/TreeViewRow.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tree-view-row">
+    <h5>TreeViewRow component</h5>
+    <!-- <PersonTag /> -->
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="css">
+.tree-view-row {
+  border-color: green;
+  border-style: dotted;
+}
+
+</style>
