44274e6e2070e82ff9392d48df42f78f8a8dd22a
jrschmidt <jrschmidt56@gmail.com>
2022-02-11 08:51:11 -0700

    treeDisplayInit() fills row 0 with root person

--- src/display-data-functions.js
+++ src/display-data-functions.js
@@ -2,7 +2,19 @@ export const treeDisplayInit = (treeData, rootPersonId, generations) => {
   let displayData = {}
   let successStatus = 'success'
 
-if (generations != 5) successStatus = 'fail'
+  if (generations != 5) successStatus = 'fail'
+
+  displayData.generationsDisplayed = generations
+  displayData.displayRootPersonId = rootPersonId
+  displayData.displayStatus = 'normal'
+
+  displayData.row0Id = []
+  displayData.row1Id = []
+  displayData.row2Id = []
+  displayData.row3Id = []
+  displayData.row4Id = []
+
+  displayData.row0Id[0] = rootPersonId
 
   displayData.result = successStatus
   return displayData
--- src/test/tree-diplay-init.test.js
+++ src/test/tree-diplay-init.test.js
@@ -7,24 +7,36 @@ describe('treeDisplayInit function', () => {
 
   test('returns a displayData object', () => {
     let displayData = treeDisplayInit(treeData, 'p0001', 5)
-    console.log(displayData)
     expect(displayData).toBeTruthy()
   })
 
   // (Temporary until values other than 5 are implemented)
-  test('gives message when "generations" parameter doesnt equal 5', () => {
+  test('gives message when `generations` parameter doesnt equal 5', () => {
     let displayData = treeDisplayInit(treeData, 'p0001', 5)
-    console.log(displayData)
     expect(displayData.result).toBe('success')
 
     displayData = treeDisplayInit(treeData, 'p0001', 4)
-    console.log(displayData)
     expect(displayData.result).toBe('fail')
 
     displayData = treeDisplayInit(treeData, 'p0001', 2)
-    console.log(displayData)
     expect(displayData.result).toBe('fail')
 
+  })
+
+  test('returns `displayStatus` of "normal" when initializing display data', () => {
+    let displayData = treeDisplayInit(treeData, 'p0001', 5)
+    expect(displayData.displayStatus).toBe('normal')
+  })
+
+  test('copies simple parameters to result', () => {
+    let displayData = treeDisplayInit(treeData, 'p0001', 5)
+    expect(displayData.generationsDisplayed).toEqual(5)
+    expect(displayData.displayRootPersonId).toBe('p0001')
+  })
+
+  test('places root display person in row 0', () => {
+    let displayData = treeDisplayInit(treeData, 'p0001', 5)
+    expect(displayData.row0Id[0]).toEqual('p0001')
   })
 
 })
