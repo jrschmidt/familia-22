bf042d23e4b2831ed88fd42f8c59ce02243cdddf
jrschmidt <jrschmidt56@gmail.com>
2022-02-11 07:54:58 -0700

    test integrity of generations parameter to treeDisplayInit() function

--- src/display-data-functions.js
+++ src/display-data-functions.js
@@ -1,3 +1,9 @@
-export const treeDisplayInit = () => {
-  return {}
+export const treeDisplayInit = (treeData, rootPersonId, generations) => {
+  let displayData = {}
+  let successStatus = 'success'
+
+if (generations != 5) successStatus = 'fail'
+
+  displayData.result = successStatus
+  return displayData
 }
--- src/test/tree-diplay-init.test.js
+++ src/test/tree-diplay-init.test.js
@@ -1,14 +1,30 @@
-import { it, describe, expect } from 'vitest'
+import { test, describe, expect } from 'vitest'
 
 import { treeData } from '../tree-data.js'
 import { treeDisplayInit } from '../display-data-functions.js'
 
 describe('treeDisplayInit function', () => {
-  it('returns a displayData object', () => {
-    const displayData = treeDisplayInit(treeData, 'p0001', 5)
-    console.log('result:')
+
+  test('returns a displayData object', () => {
+    let displayData = treeDisplayInit(treeData, 'p0001', 5)
     console.log(displayData)
     expect(displayData).toBeTruthy()
+  })
+
+  // (Temporary until values other than 5 are implemented)
+  test('gives message when "generations" parameter doesnt equal 5', () => {
+    let displayData = treeDisplayInit(treeData, 'p0001', 5)
+    console.log(displayData)
+    expect(displayData.result).toBe('success')
+
+    displayData = treeDisplayInit(treeData, 'p0001', 4)
+    console.log(displayData)
+    expect(displayData.result).toBe('fail')
+
+    displayData = treeDisplayInit(treeData, 'p0001', 2)
+    console.log(displayData)
+    expect(displayData.result).toBe('fail')
+
   })
 
 })
