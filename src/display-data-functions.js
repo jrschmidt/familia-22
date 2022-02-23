c28ad25a74f91c3ba4cefcd2978e8c17e1528b44
jrschmidt <jrschmidt56@gmail.com>
2022-02-23 12:53:59 -0700

    rename fillTree() function to growTree()

--- src/display-data-functions.js
+++ src/display-data-functions.js
@@ -1,8 +1,10 @@
 // A set of functions to generate the data necessary to render the family
 // tree display. The following functions are included:
 
+
 // treeDisplayInit()
 // Generates data needed for initial rendering of family tree display.
+
 
 // treeShiftToFather()
 // Generates data needed for moving the family tree display back one generation,
@@ -10,11 +12,13 @@
 // the root person, the root person's mother and all her ancestors, and adding
 // another generation of the new root person's ancestors.
 
+
 // treeShiftToMother()
 // Generates data needed for moving the family tree display back one generation,
 // making the mother of the previous display root person the new root, deleting
 // the root person, the root person's father and all his ancestors, and adding
 // another generation of the new root person's ancestors.
+
 
 // treeShiftToChild()
 // Generates data needed for moving the family tree display forward one
@@ -22,10 +26,25 @@
 // root, adding the root person's spouse and their ancestors, and deleting
 // the oldest generation from the display.
 
-// fillTree()
+
+// growTree()
 // A helper function to recursively populate the display tree data with
 // references to the people in the tree and their relationship to each other.
 
+
+// pairArrayElements()
+// Takes an array with an even number of elements and returns a new array
+// with the same elements in the same order, but paired into arrays with two
+// elements each. For example:
+//
+// pairArrayElements( [a,b,c,d,e,f,g,h] )
+//
+// would return
+//
+// [ [a,b], [c,d], [e,f], [g,h] ]
+//
+// This is used to pair mother/father pairs so they can be displayed next to
+// each other.
 
 
 export const treeDisplayInit = (treeData, rootPersonId, generations) => {
@@ -44,11 +63,10 @@ export const treeDisplayInit = (treeData, rootPersonId, generations) => {
     row.status = 'normal'
     row.generation = 4 - index
     row.people = []
-    // console.log(index, row.generation)
     return row
   })
 
-  fillTree(
+  growTree(
     treeData,
     displayData,
     rootPersonId,
@@ -61,31 +79,47 @@ export const treeDisplayInit = (treeData, rootPersonId, generations) => {
 }
 
 
-export const fillTree = (
+export const growTree = (
   treeData,
   displayData,
-  rootPerson,
+  startPerson,
   rootGeneration,
   endGeneration
 ) => {
-  displayData.rows[endGeneration - rootGeneration].people.push(rootPerson)
+  displayData.rows[endGeneration - rootGeneration].people.push(startPerson)
 
   if (rootGeneration != endGeneration) {
-    fillTree(
+    growTree(
       treeData,
       displayData,
-      treeData[rootPerson].fatherId,
+      treeData[startPerson].fatherId,
       rootGeneration + 1,
       endGeneration
     )
 
-    fillTree(
+    growTree(
       treeData,
       displayData,
-      treeData[rootPerson].motherId,
+      treeData[startPerson].motherId,
       rootGeneration + 1,
       endGeneration
     )
 
   }
+}
+
+
+export const pairArrayElements = (arrayIn) => {
+  let result = []
+
+  if (arrayIn.length % 2 === 0) {
+    while (arrayIn.length > 0) {
+      let p = []
+      p.push(arrayIn.shift())
+      p.push(arrayIn.shift())
+      result.push(p)
+    }
+  }
+
+  return result
 }
---
+++ src/test/pair-elements.test.js
@@ -0,0 +1,27 @@
+import { test, describe, expect } from 'vitest'
+
+import { pairArrayElements } from '../display-data-functions.js'
+
+describe('pairArrayElements function', () => {
+
+  test('returns empty array if number of elements is not even', () => {
+    const oddArray = ['p0001' ,'p0002' ,'p0003' ,'p0004' ,'p0005']
+    let resultArray = pairArrayElements(oddArray)
+    expect(resultArray).toStrictEqual( [] )
+  })
+
+})
+
+test('converts array to array of paired elements in subarrays', () => {
+  let inputArray = ['p0002' ,'p0017']
+  let resultArray = pairArrayElements(inputArray)
+  expect(resultArray).toStrictEqual( [ ['p0002', 'p0017'] ] )
+
+  inputArray = ['p0003' ,'p0004' , 'p0018', 'p0019']
+  resultArray = pairArrayElements(inputArray)
+  expect(resultArray).toStrictEqual( [ ['p0003', 'p0004'], ['p0018', 'p0019'] ] )
+
+  inputArray = ['p0005', 'p0006', 'p0007', 'p0008', 'p0020', 'p0021', 'p0022', 'p0023']
+  resultArray = pairArrayElements(inputArray)
+  expect(resultArray).toStrictEqual( [ ['p0005', 'p0006'], ['p0007', 'p0008'], ['p0020', 'p0021'], ['p0022', 'p0023'] ] )
+})
